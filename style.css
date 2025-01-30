08e2a7dea1b85aaeead8b85293874203deec28d1
IllestBrewery4580 <161643583+IllestBrewery4580@users.noreply.github.com>
2025-01-29 21:18:37 -0500

    Create style.css

---
+++ style.css
@@ -0,0 +1,43 @@
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background: linear-gradient(to bottom right, #1A0023, #004d45);
+    /* Dark Burgundy (#800020) at the top to Dark Turquoise (#355E71) at the bottom */
+    color: white; /* Text Color */
+    font-family: 'Comic Sans MS', "Comic Sans', cursive;
+}
+.passage-container {
+    text-align: center;
+}
+
+#eye {
+    display: none; /* Hidden initially */
+    width: 0;
+    height: 0;
+    background: white;
+    border-radius: 50%;
+    animation: blink 2s infinite;
+}
+
+
+@keyframes speed-up {
+    0% { animation-timing-function: linear; }
+    100% {
+        animation-timing-function: steps(10);
+        background-color: transparent;
+    }
+}
+
+.blinking-eye {
+    margin-top: 10px;
+    font-size: 2rem;
+    opacity: 0;
+    animation: blink 2s infinite;
+}
+
+@keyframes blink {
+    0%, 100% { opacity: 0; }
+    50% { opacity: 1; }
+}
